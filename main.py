4d76cfd2a4a446686b6705524f148445817356ef
Layhout <152836685+Layhout-WB@users.noreply.github.com>
2024-11-26 16:59:09 +0700

    remove comment

--- main.py
+++ main.py
@@ -3,16 +3,3 @@ from src.app import App
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
-# from PIL import Image
-# from matplotlib import pyplot as plt
-# import numpy as np
-
-# file = open(
-#     "/Users/layhout.chea/Downloads/beautiful-balloon-ste-de-1729499367.png", "rb"
-# )
-# image = Image.open(file, "r")
-
-# plt.imshow(np.asarray(image))
-
-# plt.show()
